f30103d6eca0ca3bea5a9873fc90e08c8578227a
NicolasLozanoRoa <n.lozanor2@uniandes.edu.co>
2026-07-22 16:20:32 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from datetime import datetime
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+#os.environ para despliegue. Descomente cuando ya probó todo local.
+
+
+client = MongoClient(os.environ["MONGO_URI"])
+
+
+
+db = client["ISIS2304A05202619"]
+
+
+@app.get("/")
+def inicio():
+    return {"estado": "API funcionando correctamente"}
+
+
+@app.get("/comentarios")
+def get_acomentarios():
+    return list(db["comentarios_bares"].find({},{"_id":0}))
+
+
+@app.get('/bares/{bar_id}/comentarios')
+def get_comentarios(bar_id: int):
+    comentarios = list(db["comentarios_bares"].find({"bar_id":bar_id},{"_id":0}))
+    return comentarios or {'mensaje': 'No hay ningun objeto que cumpla con esas condiciones'}
+
+
+
+@app.post('/bares/{bar_id}/comentarios')
+def post_comentario(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha']  = datetime.now().isoformat()
+    db["comentarios_bares"].insert_one(datos)
+    return {'mensaje': 'Comentario guardado'}
+
+
+#eventos
+
+@app.get("/eventos")
+def get_aeventos():
+    return list(db["eventos"].find({},{"_id":0}))
+
+# TODO: implementar GET /bares/{bar_id}/eventos
+# Debe retornar todos los eventos del bar desde la colección 'eventos'
+
+@app.get('/bares/{bar_id}/eventos')
+def get_eventos(bar_id: int):
+    eventos = list(db["eventos"].find({"bar_id":bar_id},{"_id":0}))
+    return eventos or {'mensaje': 'No hay ningun objeto que cumpla con esas condiciones'}
+
+
+# TODO: implementar POST /bares/{bar_id}/eventos  
+# Debe insertar el evento en la colección 'eventos'
+# Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
+
+@app.post('/bares/{bar_id}/eventos')
+def post_evento(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha']  = datetime.now().isoformat()
+    db["eventos"].insert_one(datos)
+    return {'mensaje': 'Comentario guardado'}
